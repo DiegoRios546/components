01f9bceae0dbfc6ca30237e57d0c95fe4520909b
Diego Rios <diegoR546@gmail.com>
2023-01-30 20:55:45 -0600

    v2

--- src/App.js
+++ src/App.js
@@ -4,14 +4,14 @@ import GoogleMap from './mapsAPI'
 
 const Button = styled.button`
   /* Adapt the colors based on primary prop */
-  background: ${props => props.utd ? "#4ea586" : props.gh ? "#171b21": props.fb ? "#4fa8f6" : "white"};
+  background: ${props => props.utd ? "#BDECB6" : props.gh ? "#6F6F6F": props.fb ? "#4fa8f6" : "white"};
   color: ${props => props.utd ? "white" : props.gh ? "#f1f6fb" : props.fb ? "#fff" : "black"};
 
   font-size: 1.5em;
   font-weight: 500;
   margin: 1em;
   padding: 0.25em 1em;
-  border: 2px solid ${props => props.utd ? "#357565" : props.gh ? "#020409" : props.fb ? "#2a63d9" : "black"};
+  border: 2px solid ${props => props.utd ? "#008F39" : props.gh ? "#020409" : props.fb ? "#2a63d9" : "black"};
   border-radius: 3px;
   cursor: pointer;
 `;
