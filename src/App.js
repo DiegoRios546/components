b74b2de0c78240a6b49726177377d337d53ca45e
Diego Rios <diegoR546@gmail.com>
2023-01-30 20:35:42 -0600

    v1

--- src/App.js
+++ src/App.js
@@ -1,28 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
-import styled from 'styled-components';
+import styled from 'styled-components'
+import './App.css'
+import GoogleMap from './mapsAPI'
 
 const Button = styled.button`
   /* Adapt the colors based on primary prop */
-  background: ${props => props.primary ? "palevioletred" : "white"};
-  color: ${props => props.primary ? "white" : "palevioletred"};
+  background: ${props => props.utd ? "#4ea586" : props.gh ? "#171b21": props.fb ? "#4fa8f6" : "white"};
+  color: ${props => props.utd ? "white" : props.gh ? "#f1f6fb" : props.fb ? "#fff" : "black"};
 
-  font-size: 1em;
+  font-size: 1.5em;
+  font-weight: 500;
   margin: 1em;
   padding: 0.25em 1em;
-  border: 2px solid palevioletred;
+  border: 2px solid ${props => props.utd ? "#357565" : props.gh ? "#020409" : props.fb ? "#2a63d9" : "black"};
   border-radius: 3px;
+  cursor: pointer;
 `;
+
 function App() {
   return (
-    <div className="App">
-    
-    <Button><h1>Rios Torres Diego Alexis</h1></Button>
-    <Button><h2>5B</h2></Button>
-    <Button><a href='https://github.com/DiegoRios546/components.git'>github</a></Button>
-    <Button primary> <a href='https://www.facebook.com/profile.php?id=100014932184855'>facebook</a></Button>
-    
+      <div className='wrapper'>
+      <h1>Botones</h1>
+      <h2>Mi nombre es: <span>Rios Torres Diego Alexis</span></h2>
+      <div className='buttons-div'>
+        <a href='https://www.utd.edu.mx'><Button utd>UTD</Button></a>
+        <a href='https://www.github.com/DiegoProtol'><Button gh>GitHub</Button></a>
+        <a href='https://www.facebook.com/profile.php?id=100014932184855'><Button fb>Red social</Button></a>
+      </div>
+      <GoogleMap />
     </div>
+    
   );
 }
 
